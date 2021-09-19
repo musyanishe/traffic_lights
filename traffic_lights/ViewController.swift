52438866ed7c4ace304231eb7e3a75816c25706f
ShevtsovVA <v.a.shevtsov@yandex.ru>
2021-09-19 12:01:31 +0300

    Initial Commit

---
+++ traffic_lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  traffic_lights
+//
+//  Created by Евгения Шевцова on 19.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
