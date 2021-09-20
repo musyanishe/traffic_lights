778e35d12537d8da1500bed52965dc4834a94949
musyanishe <wickedeyes@yandex.ru>
2021-09-20 14:47:57 +0300

    set up main storyboard

--- traffic_lights/ViewController.swift
+++ traffic_lights/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-     
+    @IBOutlet weak var redLabel: UIView!
+    @IBOutlet weak var yellowLabel: UIView!
+    @IBOutlet weak var greenLabel: UIView!
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+    enum trafficLights {
+        case red, yellow, green
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //interface of redLabel
+        redLabel.backgroundColor = .systemRed
+        redLabel.layer.cornerRadius = 75
+        redLabel.alpha = 0.3
+        
+        //interface of yellowLabel
+        yellowLabel.backgroundColor = .systemYellow
+        yellowLabel.layer.cornerRadius = 75
+        yellowLabel.alpha = 0.3
+        
+        //interface of greenLabel
+        greenLabel.backgroundColor = .systemGreen
+        greenLabel.layer.cornerRadius = 75
+        greenLabel.alpha = 0.3
+     
+        //interface of buttonLabel
+        buttonLabel.backgroundColor = .systemTeal
+        
+        
+    }
 
+    @IBAction func buttonPressed() {
+    }
+    
 }
 
